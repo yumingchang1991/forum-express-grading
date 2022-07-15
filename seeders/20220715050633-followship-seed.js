11deaa40d8bcb6a1ed82d49557187d478770c838
ChillDiver <yumingchang1991@gmail.com>
2022-07-15 13:57:55 +0800

    feat: update followship seed to not self follow

--- seeders/20220715050633-followship-seed.js
+++ seeders/20220715050633-followship-seed.js
@@ -16,12 +16,19 @@ module.exports = {
       })
       .then(async users => {
         await queryInterface.bulkInsert('Followships',
-          Array.from({ length: 8 }, () => ({
-            following_id: users[Math.floor(Math.random() * users.length)].id,
-            follower_id: users[Math.floor(Math.random() * users.length)].id,
-            created_at: new Date(),
-            updated_at: new Date()
-          }))
+          Array.from({ length: 8 }, () => {
+            const followerId = users[Math.floor(Math.random() * users.length)].id
+            let followingId = users[Math.floor(Math.random() * users.length)].id
+            while (followingId === followerId) {
+              followingId = users[Math.floor(Math.random() * users.length)].id
+            }
+            return {
+              following_id: followingId,
+              follower_id: followerId,
+              created_at: new Date(),
+              updated_at: new Date()
+            }
+          })
         )
       })
   },
