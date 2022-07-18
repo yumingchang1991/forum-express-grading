f0348c51f7b79b86633f8ff66921c2244df87384
ChillDiver <yumingchang1991@gmail.com>
2022-07-18 22:21:14 +0800

    feat: add api error handler & delete restaurant api

--- services/admin-services.js
+++ services/admin-services.js
@@ -2,12 +2,22 @@ const { Restaurant, Category } = require('../models')
 
 const adminServices = {
   getRestaurants: (req, cb) => {
-    Restaurant.findAll({
+    return Restaurant.findAll({
       raw: true,
       nest: true,
       include: [Category]
     })
       .then(restaurants => cb(null, { restaurants }))
+      .catch(err => cb(err))
+  },
+  deleteRestaurant: (req, cb) => {
+    return Restaurant
+      .findByPk(req.params.id)
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.destroy()
+      })
+      .then(deletedRestaurant => cb(null, { restaurant: deletedRestaurant }))
       .catch(err => cb(err))
   }
 }
