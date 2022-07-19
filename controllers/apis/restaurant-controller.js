ae2fb2018e777616684a17499e4702db90511c17
ChillDiver <yumingchang1991@gmail.com>
2022-07-19 15:33:06 +0800

    feat: add user-services

--- controllers/apis/restaurant-controller.js
+++ controllers/apis/restaurant-controller.js
@@ -2,7 +2,7 @@ const restaurantServices = require('../../services/restaurant-services')
 
 const restaurantController = {
   getRestaurants: (req, res, next) => {
-    restaurantServices.getRestaurants(req, (err, data) => err ? next(err) : res.json(data))
+    restaurantServices.getRestaurants(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   }
 }
 
